c7589f1305bb75d00eae44aecbb51b37b7fdaa0f
PDutriez <42210994+PDutriez@users.noreply.github.com>
2022-09-05 00:07:02 -0300

    little progress

--- src/functions.py
+++ src/functions.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from sklearn.preprocessing import OneHotEncoder
-from scipy.stats import norm, stats
+from scipy.stats import norm, gaussian_kde, multivariate_normal
 import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
+import pandas as pd
 
 def pretty_class_EDA(data,name,columns,bins=10,title=None,figsize=(16,6)):
+    """ Muestra la distribucion de los parametros de una clase y su correlacion """
     plt.figure(figsize=figsize)
     plt.subplot(1, 2, 1)
     pretty_hist(data,name,columns,bins=bins,title=f"{name} variables dist",figsize=figsize,plot=False)
@@ -13,14 +15,18 @@ def pretty_class_EDA(data,name,columns,bins=10,title=None,figsize=(16,6)):
     pretty_corr_matrix(data,name,title=f"{name} variables corr",plot=False)
     plt.show()
 
-def pretty_hist(data,name,columns,title=None,bins=10,figsize=(16,6),plot=True):
+def pretty_hist(data,name=None,columns=None,title=None,bins=10,figsize=(16,6),plot=True):
     if plot:
         plt.figure(figsize=figsize)
     if title:
         plt.title(title,fontsize=22)
     for c in columns:
-        mu, std = norm.fit(data[data["Class"] == name][c])
-        plt.hist(data[data["Class"] == name][c],bins=bins,label=f'{c}: $\mu={np.round(mu,3)},\ \sigma={np.round(std,3)}$')
+        if name:
+            mu, std = norm.fit(data[data["Class"] == name][c])
+            plt.hist(data[data["Class"] == name][c],bins=bins,label=f'{c}: $\mu={np.round(mu,3)},\ \sigma={np.round(std,3)}$')
+        else:
+            mu, std = norm.fit(data[c])
+            plt.hist(data[c],bins=bins,label=f'{c}: $\mu={np.round(mu,3)},\ \sigma={np.round(std,3)}$')
     plt.legend()
     if plot:
         plt.show()
@@ -32,23 +38,22 @@ def pretty_hist2d(data,columns,bins=10,title=None,figsize=(16,6)):
     plt.hist2d(data[columns[0]].to_numpy(),data[columns[1]].to_numpy(),bins=bins)
     plt.show()
 
-def pretty_corr_matrix(data,name,title=None,figsize=(16,6),plot=True):
+def pretty_corr_matrix(data,name=None,title=None,figsize=(16,6),plot=True):
     if plot:
         plt.figure(figsize=figsize)
     if title:
         plt.title(title,fontsize=22)
-    corr = data[data["Class"] == name].corr()
-    im = plt.imshow(data[data["Class"] == name].corr())
+    if name:
+        corr = data[data["Class"] == name].corr()
+    else:
+        corr = data.corr()
+    im = plt.matshow(corr,fignum=False)
     plt.colorbar(im)
     plt.xticks(range(len(corr.columns)), corr.columns,fontsize=12)
     plt.yticks(range(len(corr.columns)), corr.columns,fontsize=12)
-
-    # for i in range(len(corr.columns)):
-    #     for j in range(len(corr.columns)):
-    #         text = plt.text(j, i, corr.to_numpy()[i, j],
-    #                    ha="center", va="center", color="w")
     if plot:
         plt.show()
+
 def pretty_scatter(rvs,figsize=(16,6)):
     fig = plt.figure(figsize=figsize)
 
@@ -67,3 +72,59 @@ def generate_dataset(random_variables):
         y = np.vstack([y, np.ones(rv[2]).reshape(rv[2],1)*i]) 
     y = y.reshape(-1)
     return X, y
+
+def compare_param(data,column,bins=10,figsize=(16,6)):
+    """ Muestra la distribucion del parametro, el boxplot y la distribucion del parametro por clase """
+    fig, axd = plt.subplot_mosaic([['left', 'right'],['bottom', 'bottom']], figsize=figsize,constrained_layout=True)
+    # Upper Left
+    q25, q75 = np.percentile(data[column], [25, 75])
+    bin_width = 2 * (q75 - q25) * len(data[column]) ** (-1/3)
+    mu, std = norm.fit(data[column])
+    bins = round((data[column].max() - data[column].min()) / bin_width)
+    axd["left"].hist(data[column], density=True,bins = bins,label=f'{column}: $\mu={np.round(mu,3)},\ \sigma={np.round(std,3)}$')
+    mn, mx = axd["left"].set_xlim()
+    axd["left"].set_xlim(mn, mx)
+    kde_xs = np.linspace(mn, mx, 300)
+    kde = gaussian_kde(data[column])
+    axd["left"].plot(kde_xs, kde.pdf(kde_xs), label="PDF")
+    axd["left"].set_ylabel("Densidad")
+    # Upper Right
+    axd["right"].boxplot(data[column])
+    # Bottom
+    clases = data["Class"].drop_duplicates().to_list()
+    dic = {}
+    for i,name in enumerate(clases):
+        dic[name] = data[data["Class"] == name][column]
+    pd.DataFrame(dic).plot(kind="kde",ax=axd["bottom"])
+    
+# rvs = []
+# for clase in ["NUC","CYT","MIT"]:
+#     rvs.append([df[df["Class"] == clase].mean(numeric_only=True).to_list(),df[df["Class"] == clase].cov().to_numpy(),df[df["Class"] == clase].shape[0]])
+
+# nn.pretty_scatter(rvs)
+
+def get_class(sample,train,print_res=True):
+    clases = train["Class"].drop_duplicates().to_list()
+    p = {}
+    f = {}
+    x_prob = 0
+    for clase in clases:
+        p[clase] = train[train["Class"] == clase].shape[0]/train.shape[0]
+        m = train[train["Class"] == clase].mean(numeric_only=True).to_list()
+        c = train[train["Class"] == clase].cov().to_numpy()
+        f[clase] = multivariate_normal(m,c,allow_singular=True)
+        x_prob = x_prob + f[clase].pdf(sample)*p[clase]
+    
+    max_prob = 0
+    max_prob_class = None
+    for clase in clases:
+        px = f[clase].pdf(sample)*p[clase]/x_prob
+        if print_res:
+            print(f"P(Y={clase}|X) = {np.round(px,5)}")
+        if px > max_prob:
+            max_prob_class = clase
+            max_prob = px
+    if print_res:
+        print(f"clase: {max_prob_class}, P={np.round(max_prob,5)}")
+
+    return max_prob_class
